6feecbebacbc39d872a31814899b327cd636a95c
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-11-02 22:44:02 -0300

    chore(flake): remove specific directory from hosts/nixos.

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -53,12 +53,45 @@
     ../common/optionals/programs/xwayland.nix
     ../common/optionals/programs/zsh.nix
 
-    ./specific/boot.nix
-    ./specific/networking.nix
-
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "amdgpu" ];
+    initrd = {
+      # luks.devices."luks-8ff284ea-f99c-4e60-a7eb-608427b597f2".device = "/dev/disk/by-uuid/8ff284ea-f99c-4e60-a7eb-608427b597f2";
+      luks.devices."luks-eadfff45-7cad-48b0-a667-900574f082d5".device = "/dev/disk/by-uuid/eadfff45-7cad-48b0-a667-900574f082d5";
+      kernelModules = [ "amdgpu" ];
+    };
+    loader = {
+      systemd-boot.enable = true;
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+
+    firewall = {
+      allowedTCPPorts = [
+        80 # HTTP
+        443 # HTTPS
+
+        42000 # Warpinator
+        42001 # Warpinator
+      ];
+      allowedUDPPorts = [
+        42000 # Warpinator
+        42001 # Warpinator
+      ];
+      trustedInterfaces = [ "docker0" ];
+      enable = true;
+    };
+  };
 
   nixpkgs = {
     # You can add overlays here
